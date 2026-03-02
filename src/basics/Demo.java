e334c77fc6eeb481724ca01da14a807f2fd223ba
suseendar18 <suseendaran86@gmail.com>
2026-03-02 15:26:31 +0530

    first commit

---
+++ src/basics/Demo.java
@@ -0,0 +1,18 @@
+package basics;
+
+import org.openqa.selenium.JavascriptExecutor;
+import org.openqa.selenium.SearchContext;
+import org.openqa.selenium.TakesScreenshot;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+
+public class Demo {
+public static void main(String[] args) {
+	 SearchContext s;
+	 WebDriver w;
+	 JavascriptExecutor jse;
+	 TakesScreenshot t;
+	 WebElement we;
+	
+}
+}
---
+++ src/com/actitime/testscript/TaskModule.java
@@ -0,0 +1,22 @@
+package com.actitime.testscript;
+
+import org.testng.Reporter;
+import org.testng.annotations.Test;
+
+import com.actitime.generic.BaseClass;
+
+public class TaskModule extends BaseClass{
+	
+	@Test(groups="smoketest")
+public void createTask() {
+	Reporter.log("createTask",true);
+}
+	@Test(groups="regressiontest")
+public void deleteTask() {
+	Reporter.log("deleteTask",true);
+}
+	@Test(groups="regressiontest")
+public void modifyTask() {
+	Reporter.log("modifyTask",true);
+}
+}
