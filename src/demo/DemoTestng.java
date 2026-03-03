8776a58295a43c1f49ea7158a6ca7627e19b9882
luciferprasanna <draghuprasanna@gmail.com>
2026-03-02 22:09:25 -0800

    Updated demotest file

--- src/demo/DemoTestng.java
+++ src/demo/DemoTestng.java
@@ -6,7 +6,7 @@ import org.testng.annotations.Test;
 public class DemoTestng {
 	@Test
 	public static void demoB() {
-		Reporter.log("Hi World");//It will print only in the report
+		Reporter.log("I am Raghuprasanna");//It will print only in the report
 //		Reporter.log("Hi World",false);//It will print only in the report
 	}
 
